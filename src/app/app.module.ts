e8b13f8edf16a039c34984226fe633293fa0eb0d
Matan Socher <matan.socher@zoominfo.com>
2024-07-25 13:47:17 +0300

    chore: added core modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { WoltBotModule } from '@features/wolt-bot/wolt-bot.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [WoltBotModule],
   controllers: [AppController],
   providers: [AppService],
 })
