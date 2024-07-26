d0187a92c4b0ca9ad9f0049c31bbb1da538177b9
Matan Socher <matan.socher@zoominfo.com>
2024-07-26 11:50:23 +0300

    chore: remove notebook files

--- src/core/config/telegram.config.ts
+++ src/core/config/telegram.config.ts
@@ -7,10 +7,6 @@ export const BOTS = {
     name: 'Voice Pal Bot',
     token: process.env.VOICE_PAL_TELEGRAM_BOT_TOKEN,
   },
-  NOTEBOOK: {
-    name: 'Notebook Bot',
-    token: process.env.NOTEBOOK_BOT_TOKEN,
-  },
 };
 
 export const BOT_BROADCAST_ACTIONS = {
--- src/core/mongo/notebook-mongo/notebook-mongo.module.ts
+++
@@ -1,8 +0,0 @@
-import { VoicePalMongoService } from '@core/mongo/voice-pal-mongo/voice-pal-mongo.service';
-import { Module } from '@nestjs/common';
-
-@Module({
-  providers: [VoicePalMongoService],
-  exports: [VoicePalMongoService],
-})
-export class NotebookMongoModule {}
