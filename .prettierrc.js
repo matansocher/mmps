4224115a53ec0705fe3ee592165385b926a399fd
Matan Socher <matan.socher@zoominfo.com>
2024-12-17 19:34:20 +0200

    ci: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,39 @@
+const base = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 160,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
+
+module.exports = {
+  ...base,
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.cjs', '*.mjs', '*.ts', '*.tsx', '*.mts', '*.cts', '*.scss', '*.css', '*.yml', '*.yaml', '*.md', '*.json5'],
+      options: {
+        ...base,
+        semi: true,
+      },
+    },
+    {
+      files: ['*.json', '*.jsonc'],
+      options: {
+        ...base,
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: ['*.html', '*.svg'],
+      options: {
+        ...base,
+        parser: 'html',
+        singleQuote: false,
+      },
+    },
+  ],
+};
