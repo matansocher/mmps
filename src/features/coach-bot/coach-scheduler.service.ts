6b6b58e9d3196980206ca16f2005e5c521484a34
Matan Socher <matan.socher@zoominfo.com>
2024-12-23 16:50:05 +0200

    chore: clean code

--- src/features/coach-bot/coach-scheduler.service.ts
+++ src/features/coach-bot/coach-scheduler.service.ts
@@ -24,31 +24,38 @@ export class CoachBotSchedulerService {
   @Cron(`59 12,19,23 * * *`, { name: 'coach-scheduler', timeZone: DEFAULT_TIMEZONE })
   async handleIntervalFlow(): Promise<void> {
     try {
-      const competitions = await this.scores365Service.getCompetitions();
-      if (!competitions?.length) {
-        this.logger.error(this.handleIntervalFlow.name, 'error - could not get competitions');
+      const responseText = await this.getMatchesSummaryMessage();
+      if (!responseText) {
         return;
       }
-      const todayDateString = this.utilsService.getTodayDateString(new Date());
-      const competitionsWithMatches = await Promise.all(
-        competitions.map((competition) => this.scores365Service.getMatchesForCompetition(competition, todayDateString)),
-      );
-      if (!competitionsWithMatches?.length) {
-        this.logger.error(this.handleIntervalFlow.name, 'error - could not get matches');
-        return;
-      }
-
-      const competitionsWithMatchesFiltered = competitionsWithMatches.filter(({ matches }) => matches?.length);
-      if (!competitionsWithMatchesFiltered?.length) {
-        this.logger.info(this.handleIntervalFlow.name, 'no competitions with matches found');
-        return;
-      }
-      const responseText = generateMatchResultsString(competitionsWithMatchesFiltered);
       await Promise.all(this.chatIds.map((chatId) => this.telegramGeneralService.sendMessage(this.bot, chatId, responseText)));
     } catch (err) {
       const errorMessage = `error - ${this.utilsService.getErrorMessage(err)}`;
       this.logger.error(this.handleIntervalFlow.name, errorMessage);
       this.telegramGeneralService.sendMessage(this.bot, MY_USER_ID, errorMessage);
     }
+  }
+
+  async getMatchesSummaryMessage(): Promise<string> {
+    const competitions = await this.scores365Service.getCompetitions();
+    if (!competitions?.length) {
+      this.logger.error(this.handleIntervalFlow.name, 'error - could not get competitions');
+      return;
+    }
+    const todayDateString = this.utilsService.getTodayDateString(new Date());
+    const competitionsWithMatches = await Promise.all(
+      competitions.map((competition) => this.scores365Service.getMatchesForCompetition(competition, todayDateString)),
+    );
+    if (!competitionsWithMatches?.length) {
+      this.logger.error(this.handleIntervalFlow.name, 'error - could not get matches');
+      return;
+    }
+
+    const competitionsWithMatchesFiltered = competitionsWithMatches.filter(({ matches }) => matches?.length);
+    if (!competitionsWithMatchesFiltered?.length) {
+      this.logger.info(this.handleIntervalFlow.name, 'no competitions with matches found');
+      return;
+    }
+    return generateMatchResultsString(competitionsWithMatchesFiltered);
   }
 }
