8d30e35d2a2ce7f6ede70d85f29f183b817a4ee6
Matan Socher <matansocher@gmail.com>
2024-12-28 19:32:27 +0200

    chore: remove scraping google search and playwright

--- src/features/teacher-bot/teacher-scheduler.service.ts
+++ src/features/teacher-bot/teacher-scheduler.service.ts
@@ -5,9 +5,7 @@ import { DEFAULT_TIMEZONE } from '@core/config';
 import { LoggerService } from '@core/logger';
 import { MY_USER_ID } from '@core/notifier-bot/notifier-bot.config';
 import { UtilsService } from '@core/utils';
-import { type GoogleArticle, GoogleSearchService } from '@services/google-search';
 import { OpenaiAssistantService } from '@services/openai';
-import { ScrapingService } from '@services/scraping/scraping.service';
 import { BOTS, TelegramGeneralService } from '@services/telegram';
 import {
   TEACHER_ASSISTANT_ID,
@@ -25,8 +23,6 @@ export class TeacherSchedulerService {
     private readonly logger: LoggerService,
     private readonly utilsService: UtilsService,
     private readonly openaiAssistantService: OpenaiAssistantService,
-    private readonly googleSearchService: GoogleSearchService,
-    private readonly scrapingService: ScrapingService,
     private readonly telegramGeneralService: TelegramGeneralService,
     @Inject(BOTS.TEACHER.name) private readonly bot: TelegramBot,
   ) {}
@@ -34,51 +30,43 @@ export class TeacherSchedulerService {
   @Cron(`0 ${HOURS_OF_DAY.join(',')} * * *`, { name: 'teacher-scheduler', timeZone: DEFAULT_TIMEZONE })
   async handleIntervalFlow(topic: string): Promise<void> {
     try {
-      const rawArticles = await this.googleSearchService.getGoogleSearchResults(topic);
-      if (!rawArticles?.length) {
-        return;
-      }
+      // const rawArticles = await this.googleSearchService.getGoogleSearchResults(topic);
+      // if (!rawArticles?.length) {
+      //   return;
+      // }
 
-      const articlesContents: GoogleArticle[] = await Promise.all(
-        rawArticles.map(async (article: GoogleArticle) => {
-          const htmlContent = await this.scrapingService.getArticleContent(article.link);
-          const content = this.scrapingService.getTextFromHtml(htmlContent);
-          return { ...article, content };
-        }),
-      );
+      // const articlesContents: GoogleArticle[] = await Promise.all(
+      //   rawArticles.map(async (article: GoogleArticle) => {
+      //     const htmlContent = await this.scrapingService.getArticleContent(article.link);
+      //     const content = this.scrapingService.getTextFromHtml(htmlContent);
+      //     return { ...article, content };
+      //   }),
+      // );
 
-      const articles = articlesContents.filter((article: GoogleArticle) => !!article?.content?.length);
-      if (!articles.length) {
-        return;
-      }
+      // const articles = articlesContents.filter((article: GoogleArticle) => !!article?.content?.length);
+      // if (!articles.length) {
+      //   return;
+      // }
 
-      const filePath = await this.writeContentsToFile(articles);
+      // const filePath = await this.writeContentsToFile(articles);
 
-      const openAIFileID = await this.openaiAssistantService.uploadFile(filePath);
+      // const openAIFileID = await this.openaiAssistantService.uploadFile(filePath);
 
-      const summary = await this.getSummary(openAIFileID, topic);
+      // const summary = await this.getSummary(openAIFileID, topic);
 
-      await Promise.all(this.chatIds.map((chatId) => this.telegramGeneralService.sendMessage(this.bot, chatId, summary)));
+      // await Promise.all(this.chatIds.map((chatId) => this.telegramGeneralService.sendMessage(this.bot, chatId, summary)));
 
-      await Promise.all([this.openaiAssistantService.deleteFile(openAIFileID), this.utilsService.deleteFile(filePath)]);
+      // await Promise.all([this.openaiAssistantService.deleteFile(openAIFileID), this.utilsService.deleteFile(filePath)]);
     } catch (err) {
       this.logger.error(this.handleIntervalFlow.name, `error in handleIntervalFlow: ${this.utilsService.getErrorMessage(err)}`);
       return;
     }
   }
 
-  async writeContentsToFile(articlesContents: GoogleArticle[]): Promise<string> {
-    const filePath = `${LOCAL_FILES_PATH}/teacher-articles-${Date.now()}.txt`;
-    const fileContent = articlesContents.map(({ title, link, content }, i) => `#${i + 1}\n${title}\n${link}\n${content}\n`).join('\n\n');
-    const compressedContent = fileContent.slice(0, MAX_NUMBER_OF_CHARS_PER_FILE);
-    await this.utilsService.writeFile(filePath, compressedContent);
-    return filePath;
-  }
-
-  async getSummary(openAIFileID: string, topic: string): Promise<string> {
-    const threadId = await this.openaiAssistantService.createThread();
-    await this.openaiAssistantService.addMessageToThread(threadId, `please help me learn ${topic} with the given data in the file`, 'user', openAIFileID);
-    const threadRun = await this.openaiAssistantService.runThread(TEACHER_ASSISTANT_ID, threadId);
-    return this.openaiAssistantService.getThreadResponse(threadRun.thread_id);
-  }
+  // async getSummary(openAIFileID: string, topic: string): Promise<string> {
+  //   const threadId = await this.openaiAssistantService.createThread();
+  //   await this.openaiAssistantService.addMessageToThread(threadId, `please help me learn ${topic} with the given data in the file`, 'user', openAIFileID);
+  //   const threadRun = await this.openaiAssistantService.runThread(TEACHER_ASSISTANT_ID, threadId);
+  //   return this.openaiAssistantService.getThreadResponse(threadRun.thread_id);
+  // }
 }
--- src/services/google-search/interface/index.ts
+++
@@ -1,6 +0,0 @@
-export interface GoogleArticle {
-  readonly title: string;
-  readonly link: string;
-  readonly snippet?: string;
-  readonly content?: string;
-}
