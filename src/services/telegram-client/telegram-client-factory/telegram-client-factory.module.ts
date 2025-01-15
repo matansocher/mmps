0baed3fdbc5199df19d6259591e99587805a5e55
Matan Socher <matan.socher@zoominfo.com>
2025-01-15 14:24:39 +0200

    chore: use config service for telegram client

--- src/services/telegram-client/telegram-client-factory/telegram-client-factory.module.ts
+++ src/services/telegram-client/telegram-client-factory/telegram-client-factory.module.ts
@@ -1,16 +1,22 @@
 import { TelegramClient } from 'telegram';
 import { StringSession } from 'telegram/sessions';
 import { Module, DynamicModule, Global } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TELEGRAM_CLIENT_TOKEN } from '@services/telegram-client';
 import { ITelegramClientFactoryOptions } from '../interface';
 
 @Global()
 @Module({})
 export class TelegramClientFactoryModule {
   static forChild(options: ITelegramClientFactoryOptions): DynamicModule {
-    const { name, apiId, apiHash, stringSession, connectionRetries = 5 } = options;
+    const { connectionRetries = 5 } = options;
     const TelegramClientProvider = {
-      provide: name,
-      useFactory: async (): Promise<TelegramClient> => {
+      provide: TELEGRAM_CLIENT_TOKEN,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService): Promise<TelegramClient> => {
+        const apiId = configService.getOrThrow<number>('TELEGRAM_API_ID');
+        const apiHash = configService.getOrThrow<string>('TELEGRAM_API_HASH');
+        const stringSession = configService.getOrThrow<string>('TELEGRAM_STRING_SESSION');
         const client = new TelegramClient(new StringSession(stringSession), apiId, apiHash, { connectionRetries });
         await client.start({ phoneNumber: null, password: null, phoneCode: null, onError: (err) => console.log(err) });
         return client;
