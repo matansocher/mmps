fa256d4d5112e7553d5c439aa97c1e75d7dfd9bc
Matan Socher <matan.socher@zoominfo.com>
2025-01-15 14:18:35 +0200

    chore: use config service for gemini token

--- src/services/gemini/gemini.config.ts
+++ src/services/gemini/gemini.config.ts
@@ -1,6 +1,3 @@
-import { env } from 'node:process';
-
 export const GENERATIVE_MODEL_CLIENT_TOKEN = 'GENERATIVE_MODEL_CLIENT_TOKEN';
 
-export const GEMINI_API_KEY = env.GEMINI_API_KEY;
 export const GEMINI_FLASH_MODEL = 'gemini-1.5-flash';
--- src/services/gemini/gemini.module.ts
+++ src/services/gemini/gemini.module.ts
@@ -1,13 +1,15 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { type FactoryProvider, Module } from '@nestjs/common';
-import { GEMINI_API_KEY, GEMINI_FLASH_MODEL, GENERATIVE_MODEL_CLIENT_TOKEN } from './gemini.config';
+import { ConfigService } from '@nestjs/config';
+import { GEMINI_FLASH_MODEL, GENERATIVE_MODEL_CLIENT_TOKEN } from './gemini.config';
 import { GeminiService } from './gemini.service';
 import { IGeminiClientProvider } from './interface';
 
 export const GenerativeModelClientProvider: FactoryProvider = {
   provide: GENERATIVE_MODEL_CLIENT_TOKEN,
-  useFactory: (): IGeminiClientProvider => {
-    const generativeAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): IGeminiClientProvider => {
+    const generativeAI = new GoogleGenerativeAI(configService.get('GEMINI_API_KEY'));
     const flashModel = generativeAI.getGenerativeModel({ model: GEMINI_FLASH_MODEL });
     return { flashModel };
   },
