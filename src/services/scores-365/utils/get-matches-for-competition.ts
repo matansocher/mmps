1c63b7902b72433d0153f98c988bd582e86f4cb5
Matan Socher <matansocher@gmail.com>
2025-01-30 01:13:01 +0200

    chore: coach bug

--- src/services/scores-365/utils/get-matches-for-competition.ts
+++ src/services/scores-365/utils/get-matches-for-competition.ts
@@ -6,9 +6,13 @@ import { LANGUAGE_ID, SCORES_365_API_URL } from '../scores-365.config';
 import { getMatchDetails } from './get-match-details';
 
 export async function getMatchesForCompetition(competition: Competition, date: string): Promise<CompetitionDetails> {
-  const queryParams = { competitions: competition.id.toString(), langId: `${LANGUAGE_ID}`, timezoneName: DEFAULT_TIMEZONE };
+  const queryParams = {
+    competitions: competition.id.toString(),
+    langId: `${LANGUAGE_ID}`,
+    timezoneName: DEFAULT_TIMEZONE,
+  };
   const allMatchesRes = await axios.get(`${SCORES_365_API_URL}/games/current?${new URLSearchParams(queryParams)}`);
-  const matchesRes = allMatchesRes?.data?.games.filter((matchRes: ExpectedMatch) => date === getDateString(new Date(matchRes.startTime)));
+  const matchesRes = allMatchesRes?.data?.games?.filter((matchRes: ExpectedMatch) => date === getDateString(new Date(matchRes.startTime))) || [];
   const enrichedMatches = await Promise.all(matchesRes.map((matchRes: MatchDetails) => getMatchDetails(matchRes.id)));
   return { competition, matches: enrichedMatches.filter(Boolean) };
 }
