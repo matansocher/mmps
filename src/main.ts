1afb1c3a778b041de9e03f9aca2bb621eb7c4ba9
Matan Socher <matansocher@gmail.com>
2025-01-30 21:12:24 +0200

    chore: service name

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import * as bodyParser from 'body-parser';
 import { configDotenv } from 'dotenv';
 import { env } from 'node:process';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 configDotenv();
-
-console.log(process.versions.node);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -20,9 +19,12 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
+  const config = new DocumentBuilder().setTitle('mmps').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
+  const logger = new Logger('main.ts');
+  logger.log(process.versions.node);
 
   await app.listen(env.PORT || 3000);
 }
