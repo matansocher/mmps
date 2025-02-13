ad58e388411acd3ba43ac48b97669a648139a043
Matan Socher <matansocher@gmail.com>
2025-02-13 21:29:02 +0200

    feat(trainer): process message template

--- src/features/trainer-bot/trainer.service.ts
+++ src/features/trainer-bot/trainer.service.ts
@@ -2,7 +2,16 @@ import type TelegramBot from 'node-telegram-bot-api';
 import { Inject, Injectable } from '@nestjs/common';
 import { TrainerMongoExerciseService } from '@core/mongo/trainer-mongo';
 import { BOTS } from '@services/telegram';
-import { generateExerciseEncourageMessage, generateWeeklySummaryMessage, getLastWeekDates, getLongestStreak, getStreak } from './utils';
+import {
+  EXERCISE_ENCOURAGE_MESSAGES,
+  EXERCISE_REPLY_MESSAGES,
+  getLastWeekDates,
+  getLongestStreak,
+  getStreak,
+  processMessageTemplate,
+  SPECIAL_STREAKS_MESSAGES,
+  WEEKLY_SUMMARY_MESSAGES,
+} from './utils';
 
 @Injectable()
 export class TrainerService {
@@ -23,8 +32,21 @@ export class TrainerService {
     }
     const exercisesDates = await this.getExercisesDates(chatId);
     const currentStreak = getStreak(exercisesDates);
-    const replyText = generateExerciseEncourageMessage({ currentStreak });
+    const template = EXERCISE_ENCOURAGE_MESSAGES[Math.floor(Math.random() * EXERCISE_ENCOURAGE_MESSAGES.length)];
+    const replyText = processMessageTemplate(template, { currentStreak });
     await this.bot.sendMessage(chatId, replyText);
+  }
+
+  getExerciseReplyText({ currentStreak, longestStreak }): string {
+    const specialSteaks = Object.keys(SPECIAL_STREAKS_MESSAGES);
+    currentStreak = 7;
+    if (specialSteaks.includes(`${currentStreak}`)) {
+      const relevantMessages = SPECIAL_STREAKS_MESSAGES[currentStreak];
+      const specialStreakTemplate = relevantMessages[Math.floor(Math.random() * relevantMessages.length)];
+      return processMessageTemplate(specialStreakTemplate, { currentStreak });
+    }
+    const exerciseTemplate = EXERCISE_REPLY_MESSAGES[Math.floor(Math.random() * EXERCISE_REPLY_MESSAGES.length)];
+    return processMessageTemplate(exerciseTemplate, { currentStreak, longestStreak });
   }
 
   async processWeeklySummary(chatId: number): Promise<void> {
@@ -38,7 +60,8 @@ export class TrainerService {
     const currentStreak = getStreak(lastWeekExercises);
     const longestStreak = getLongestStreak(exercisesDates);
 
-    const summaryMessage = generateWeeklySummaryMessage({ totalExercises, currentStreak, longestStreak });
+    const template = WEEKLY_SUMMARY_MESSAGES[Math.floor(Math.random() * WEEKLY_SUMMARY_MESSAGES.length)];
+    const summaryMessage = processMessageTemplate(template, { totalExercises, currentStreak, longestStreak });
 
     await this.bot.sendMessage(chatId, summaryMessage);
   }
