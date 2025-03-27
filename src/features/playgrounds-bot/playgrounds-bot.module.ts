d0877743d20f1d04f2d6010fab34ef4ed648a99c
Matan Socher <matansocher@gmail.com>
2025-03-27 23:43:26 +0200

    chore: structured response example

--- src/features/playgrounds-bot/playgrounds-bot.module.ts
+++ src/features/playgrounds-bot/playgrounds-bot.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { OpenaiModule } from '@services/openai';
 import { BOTS, TelegramBotsFactoryProvider } from '@services/telegram';
 import { PlaygroundsBotService } from './playgrounds-bot.service';
 
 @Module({
+  imports: [OpenaiModule],
   providers: [PlaygroundsBotService, TelegramBotsFactoryProvider(BOTS.PLAYGROUNDS)],
 })
 export class PlaygroundsBotModule {}
