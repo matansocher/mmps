2b9559a1c65a93a4ef16f0b5681bcdbc67e64380
Matan Socher <matansocher@gmail.com>
2025-04-17 22:48:09 +0300

    chore: create table template

--- src/services/telegram/utils/get-table-template.ts
+++ src/services/telegram/utils/get-table-template.ts
@@ -6,17 +6,17 @@ type TableItems = {
 export function getTableTemplate(items: Array<TableItems>): string {
   const indices = items.map((_, i) => String(i + 1));
   const names = items.map((item) => item.name);
-  const pts = items.map((item) => String(item.value));
+  const values = items.map((item) => String(item.value));
 
   const idxWidth = Math.max(...indices.map((s) => s.length));
   const nameWidth = Math.max(...names.map((s) => s.length));
-  const ptsWidth = Math.max(...pts.map((s) => s.length));
+  const valuesWidth = Math.max(...values.map((s) => s.length));
 
   const rows = items.map((item, i) => {
     const idx = String(i + 1).padStart(idxWidth, ' ');
     const name = item.name.padEnd(nameWidth, ' ');
-    const pt = String(item.value).padStart(ptsWidth, ' ');
-    return `${idx}  ${name}  ${pt}`;
+    const val = String(item.value).padStart(valuesWidth, ' ');
+    return `${idx}  ${name}  ${val}`;
   });
 
   const table = rows.join('\n');
