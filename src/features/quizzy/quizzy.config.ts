7892693a91b487848097847e32862073c1288cda
Matan Socher <matansocher@gmail.com>
2025-06-14 21:40:27 +0300

    feat(quizzy): add thread with asistant for questions

--- src/features/quizzy/quizzy.config.ts
+++ src/features/quizzy/quizzy.config.ts
@@ -17,6 +17,7 @@ export const ANALYTIC_EVENT_NAMES = {
   CONTACT: 'CONTACT',
   GAME: 'GAME',
   ANSWERED: 'ANSWERED',
+  MESSAGE: 'MESSAGE',
   ERROR: 'ERROR',
 };
 
@@ -26,3 +27,15 @@ export enum BOT_ACTIONS {
   CONTACT = 'contact',
   GAME = 'game',
 }
+
+export const QUIZZY_ASSISTANT_ID = 'asst_QcNCuLWYO5QctyFeacTa79dA';
+export const QUIZZY_STRUCTURED_RES_START = 'create a question for the user';
+export const QUIZZY_STRUCTURED_RES_INSTRUCTIONS = `
+You are generating trivia questions for a Telegram bot. Each question should be multiple-choice with 1 correct answer and 3 plausible incorrect answers. Focus on **medium to hard** difficulty. The goal is to help users learn and expand their knowledge.
+
+- Generate questions from various categories such as: Israeli history, culture, geography, language, inventions, global history, science, literature, philosophy, politics (historical), and nature.
+- Avoid easy or obvious questions. Make sure each question teaches something interesting or less commonly known.
+- Make incorrect answers believable and relevant to the topic (not silly or easily dismissible).
+- Avoid opinion-based or ambiguous questions.
+- always provide data in hebrew as the users are hebrew speakers.
+`;
--- src/features/quizzy/tools/index.ts
+++
@@ -1,1 +0,0 @@
-export { triviaTool } from './trivia';
