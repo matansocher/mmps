0c592091e233b76f74f971de1e88330447b6e59a
Matan Socher <matansocher@gmail.com>
2025-06-17 01:17:34 +0300

    chore: better search in wolt

--- src/features/wolt/utils/get-restaurants-by-name.ts
+++ src/features/wolt/utils/get-restaurants-by-name.ts
@@ -1,10 +1,23 @@
 import { WoltRestaurant } from '../interface';
 import { CITIES_SLUGS_SUPPORTED, MAX_NUM_OF_RESTAURANTS_TO_SHOW } from '../wolt.config';
 
+function normalize(str: string): string {
+  return str
+    .toLowerCase()
+    .replace(/['"`\-]/g, '') // remove common special characters
+    .replace(/\s+/g, ' ') // normalize whitespace (optional)
+    .trim();
+}
+
 // sort by the order of areas in CITIES_SLUGS_SUPPORTED
 export function getRestaurantsByName(restaurants: WoltRestaurant[], searchInput: string): WoltRestaurant[] {
-  return restaurants
-    .filter((restaurant: WoltRestaurant) => restaurant.name.toLowerCase().includes(searchInput.toLowerCase()))
-    .sort((a: WoltRestaurant, b: WoltRestaurant) => CITIES_SLUGS_SUPPORTED.indexOf(a.area) - CITIES_SLUGS_SUPPORTED.indexOf(b.area))
-    .slice(0, MAX_NUM_OF_RESTAURANTS_TO_SHOW);
+  const normalizedSearch = normalize(searchInput);
+
+  return (
+    restaurants
+      // .filter((restaurant: WoltRestaurant) => restaurant.name.toLowerCase().includes(searchInput.toLowerCase()))
+      .filter((restaurant: WoltRestaurant) => normalize(restaurant.name).includes(normalizedSearch))
+      .sort((a: WoltRestaurant, b: WoltRestaurant) => CITIES_SLUGS_SUPPORTED.indexOf(a.area) - CITIES_SLUGS_SUPPORTED.indexOf(b.area))
+      .slice(0, MAX_NUM_OF_RESTAURANTS_TO_SHOW)
+  );
 }
