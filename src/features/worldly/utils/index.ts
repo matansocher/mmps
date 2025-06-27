47a3e47f8304069cf93c6455cb2da3f4892c11e0
Matan Socher <matansocher@gmail.com>
2025-06-27 14:20:09 +0300

    feat(worldly): add statistics action (#185)

--- src/features/worldly/utils/index.ts
+++ src/features/worldly/utils/index.ts
@@ -1,7 +1,7 @@
 export { generateCountryMap } from './generate-country-map';
-export { generateSpecialMessage } from './generate-special-message';
+export { generateSpecialMessage, generateStatisticsMessage } from './generate-special-message';
 export { getCapitalDistractors, getMapStateDistractors, getFlagDistractors, getMapDistractors } from './get-distractors';
 export { getCountries, getStates, getRandomState, getCountryByCapital, getCountryByName, getRandomCountry, getStateByName } from './get-countries';
 export { getCountryMap } from './get-country-map';
 export { simplifyCountryName } from './simplify-country-name';
-export { getStreak } from './streak-calc';
+export { getLongestStreak, getStreak, getStreakOfCorrectAnswers } from './streak-calc';
