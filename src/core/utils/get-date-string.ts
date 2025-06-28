adf67d3c4ba9e2342efbb0a925f5ab7c3953a5ac
Matan Socher <matansocher@gmail.com>
2025-06-28 13:36:52 +0300

    chore: dates bug fix

--- src/core/utils/get-date-string.ts
+++ src/core/utils/get-date-string.ts
@@ -3,6 +3,6 @@ import { DEFAULT_TIMEZONE } from '@core/config';
 import { getDateNumber } from './get-date-number';
 
 export function getDateString(date?: Date): string {
-  const finalDate = date || toZonedTime(new Date(), DEFAULT_TIMEZONE);
+  const finalDate = toZonedTime(date || new Date(), DEFAULT_TIMEZONE);
   return `${finalDate.getFullYear()}-${getDateNumber(finalDate.getMonth() + 1)}-${getDateNumber(finalDate.getDate())}`;
 }
