15c1f7246cdc4bdc49cbf3bd8e1d28934eb716c4
Matan Socher <matansocher@gmail.com>
2025-07-05 01:52:50 +0300

    feat(worldly): add fire mode feature

---
+++ src/features/worldly/cache/user-preferences-cache.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { BaseCache } from '@core/services';
+
+export type UserPreferences = {
+  readonly onFireMode: boolean; // this mode is sending the user a new game immediately after finishing the previous one
+};
+
+const validForMinutes = 30;
+
+@Injectable()
+export class UserPreferencesCacheService extends BaseCache<UserPreferences> {
+  constructor() {
+    super(validForMinutes);
+  }
+
+  getUserPreferences(chatId: number): UserPreferences | null {
+    return this.getFromCache(chatId.toString());
+  }
+
+  saveUserPreferences(chatId: number, data: UserPreferences): void {
+    this.saveToCache(chatId.toString(), data);
+  }
+}
