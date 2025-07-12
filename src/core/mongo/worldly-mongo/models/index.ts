dce9636efa5b4af4b38ca8a7e5c64f3adc662d5f
Matan Socher <matansocher@gmail.com>
2025-07-13 00:14:04 +0300

    feat(worldly): start using mongo instead of huge json (#207)

--- src/core/mongo/worldly-mongo/models/index.ts
+++ src/core/mongo/worldly-mongo/models/index.ts
@@ -1,2 +1,4 @@
+export type { Country } from './country.model';
 export type { GameLog } from './game-log.model';
 export type { Subscription } from './subscription.model';
+export type { State } from './state.model';
--- src/features/worldly/utils/generate-area-map.ts
+++ src/features/worldly/utils/generate-area-map.ts
@@ -1,15 +1,15 @@
 import { Canvas, CanvasRenderingContext2D, createCanvas } from 'canvas';
-import { getCountries, getStates, simplifyCountryName } from '.';
-import { Country, State } from '../types';
+import { Country, State } from '@core/mongo/worldly-mongo';
+import { simplifyAreaName } from '.';
 
-type GenerateCountryOptions = {
+type GenerateAreaOptions = {
   readonly strokeColor: string;
   readonly fillColor?: string;
   readonly lineWidth?: number;
   readonly shouldFill?: boolean;
 };
 
-type Area = Country | State;
+export type Area = Country | State;
 
 const WIDTH = 800;
 const HEIGHT = 800;
@@ -19,9 +19,9 @@ const US_CENTER_LON = -96.726486;
 const US_CENTER_LAT = 38.5266;
 
 const COLORS = {
-  COUNTRY_BORDER_HIGHLIGHTED: '#FF0000',
-  COUNTRY_BORDER: '#000000',
-  COUNTRY_LAND: '#D3D3D3',
+  AREA_BORDER_HIGHLIGHTED: '#FF0000',
+  AREA_BORDER: '#000000',
+  AREA_LAND: '#D3D3D3',
   OCEAN: '#77afe0',
 };
 
@@ -34,8 +34,8 @@ function project(lon: number, lat: number, zoom = ZOOM, centerLon: number, cente
   return [x, y];
 }
 
-function drawCountry(ctx: CanvasRenderingContext2D, country: Area, coordsCountry: Area, isState: boolean, { strokeColor, fillColor, shouldFill = false, lineWidth = 1 }: GenerateCountryOptions) {
-  const { zoom, lon: centerLon, lat: centerLat } = country;
+function drawArea(ctx: CanvasRenderingContext2D, area: Area, coordsArea: Area, isState: boolean, { strokeColor, fillColor, shouldFill = false, lineWidth = 1 }: GenerateAreaOptions) {
+  const { zoom, lon: centerLon, lat: centerLat } = area;
 
   ctx.beginPath();
   if (fillColor) {
@@ -44,7 +44,7 @@ function drawCountry(ctx: CanvasRenderingContext2D, country: Area, coordsCountry
   ctx.strokeStyle = strokeColor;
   ctx.lineWidth = lineWidth;
 
-  const coords = coordsCountry.geometry.type === 'Polygon' ? [coordsCountry.geometry.coordinates] : coordsCountry.geometry.coordinates;
+  const coords = coordsArea.geometry.type === 'Polygon' ? [coordsArea.geometry.coordinates] : coordsArea.geometry.coordinates;
   coords.forEach((polygon) => {
     polygon.forEach((ring: number[][], ringIdx: number) => {
       ring.forEach(([lon, lat], i) => {
@@ -60,10 +60,9 @@ function drawCountry(ctx: CanvasRenderingContext2D, country: Area, coordsCountry
   ctx.stroke();
 }
 
-export function generateCountryMap(countryName: string, isState = false): Canvas {
-  const countries = isState ? getStates() : getCountries();
-  const country = countries.find((c) => simplifyCountryName(c.name) === simplifyCountryName(countryName));
-  if (!country) {
+export function generateAreaMap(allAreas: Area[], areaName: string, isState = false): Canvas {
+  const area = allAreas.find((c) => simplifyAreaName(c.name) === simplifyAreaName(areaName));
+  if (!area) {
     return undefined;
   }
   const canvas = createCanvas(WIDTH, HEIGHT);
@@ -74,15 +73,15 @@ export function generateCountryMap(countryName: string, isState = false): Canvas
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
   // Draw all countries (land and boundaries)
-  countries.forEach((currentCountry) => {
-    if (!currentCountry.geometry) {
+  allAreas.forEach((currentArea) => {
+    if (!currentArea.geometry) {
       return;
     }
-    drawCountry(ctx, country, currentCountry, isState, { shouldFill: true, fillColor: COLORS.COUNTRY_LAND, strokeColor: COLORS.COUNTRY_BORDER });
+    drawArea(ctx, area, currentArea, isState, { shouldFill: true, fillColor: COLORS.AREA_LAND, strokeColor: COLORS.AREA_BORDER });
   });
 
-  // Draw the selected country
-  drawCountry(ctx, country, country, isState, { lineWidth: 3, strokeColor: COLORS.COUNTRY_BORDER_HIGHLIGHTED });
+  // Draw the selected area
+  drawArea(ctx, area, area, isState, { lineWidth: 3, strokeColor: COLORS.AREA_BORDER_HIGHLIGHTED });
 
   return canvas;
 }
