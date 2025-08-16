b43a9f427a2934af227fb51fd22ef276d1897a64
Matan Socher <matansocher@gmail.com>
2025-08-16 14:08:29 +0300

    chore: move cooker bot to notifier bot

---
+++ src/core/notifier/cooker/index.ts
@@ -0,0 +1,3 @@
+export { CookerService } from './cooker.service';
+export { generateRecipeString } from './generate-recipe-string';
+export { RecipesCacheService } from './recipes-cache.service';
--- src/core/notifier/notifier.module.ts
+++ src/core/notifier/notifier.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { CookerMongoModule } from '@core/mongo/cooker-mongo';
 import { WoltMongoModule } from '@core/mongo/wolt-mongo';
 import { WorldlyMongoModule } from '@core/mongo/worldly-mongo';
 import { TelegramBotsFactoryProvider } from '@services/telegram';
+import { CookerService, RecipesCacheService } from './cooker';
 import { BOT_CONFIG } from './notifier.config';
 import { NotifierController } from './notifier.controller';
 import { NotifierService } from './notifier.service';
 
 @Module({
-  imports: [WoltMongoModule, WorldlyMongoModule],
-  providers: [NotifierController, NotifierService, TelegramBotsFactoryProvider(BOT_CONFIG)],
+  imports: [WoltMongoModule, WorldlyMongoModule, CookerMongoModule],
+  providers: [NotifierController, NotifierService, CookerService, RecipesCacheService, TelegramBotsFactoryProvider(BOT_CONFIG)],
   exports: [NotifierService],
 })
 export class NotifierModule {}
