af27547c07a42f7d8b67042c6503cc6ed63138d5
Matan Socher <matansocher@gmail.com>
2025-09-06 17:30:34 +0300

    feat(worldly): remove special messages

--- src/features/worldly/utils/index.ts
+++ src/features/worldly/utils/index.ts
@@ -1,5 +1,5 @@
 export { generateAreaMap } from './generate-area-map';
-export { generateSpecialMessage, generateStatisticsMessage } from './generate-special-message';
+export { generateStatisticsMessage } from './generate-special-message';
 export { getCapitalDistractors, getMapStateDistractors, getFlagDistractors, getMapDistractors } from './get-distractors';
 export { getAreaMap } from './get-area-map';
 export { simplifyAreaName } from './simplify-area-name';
