a9077c1ba718621f258a19b61c4e2ab7406ed940
Matan Socher <matansocher@gmail.com>
2025-09-12 02:46:30 +0300

    feat(sr): added semantic release to repo

--- src/services/google-calendar/types.ts
+++ src/services/google-calendar/types.ts
@@ -37,16 +37,3 @@ export interface CalendarListOptions {
   q?: string; // Free text search terms
   singleEvents?: boolean;
 }
-
-export interface CalendarCredentials {
-  type: string;
-  project_id: string;
-  private_key_id: string;
-  private_key: string;
-  client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
-}
