309937733ebcc53a51417dbcca2328391c88c0e8
Matan Socher <matansocher@gmail.com>
2025-09-20 22:23:18 +0300

    feat(chatbot): remove educator (#247)

--- src/features/chatbot/utils/educator/index.ts
+++
@@ -1,9 +0,0 @@
-export { startNewTopic, EducatorResponse } from './start-new-topic';
-export { continueTopicDiscussion } from './continue-topic-discussion';
-export { completeTopic } from './complete-topic';
-export { getLearningProgress } from './get-learning-progress';
-export { toggleDailyLessons } from './toggle-daily-lessons';
-export { getTopicSummaries } from './get-topic-summaries';
-export { getActiveTopic } from './get-active-topic';
-export { processDailyTopic } from './process-daily-topic';
-export { sendTopicReminder } from './send-topic-reminder';
