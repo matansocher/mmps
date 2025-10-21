3555b76eaac7d655af76f5f8cf81c4209af2c1be
Matan Socher <matansocher@gmail.com>
2025-10-22 01:45:06 +0300

    feat(chatbot): sports scheduler evening (#281)

--- src/features/chatbot/schedulers/index.ts
+++ src/features/chatbot/schedulers/index.ts
@@ -2,5 +2,6 @@ export { dailySummary } from './daily-summary';
 export { exerciseReminder } from './exercise-reminder';
 export { footballPredictions } from './football-predictions';
 export { footballUpdate } from './football-update';
+export { footballUpdateEvening } from './football-update-evening';
 export { sportsCalendar } from './sports-calendar';
 export { weeklyExerciseSummary } from './weekly-exercise-summary';
