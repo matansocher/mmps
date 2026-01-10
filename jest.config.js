6055c4eb3c47fa2a3298d3b392c348011b175719
Matan Socher <matansocher@gmail.com>
2026-01-10 22:18:08 +0200

    chore: fix

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,14 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+  },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
