953d4f995315ea683482512d4b7c0e4ad8165aa3
Matan Socher <matansocher@gmail.com>
2026-03-22 00:13:03 +0200

    feat(chatbot): clean schedulers

--- src/features/chatbot/chatbot-scheduler.service.ts
+++ src/features/chatbot/chatbot-scheduler.service.ts
@@ -2,20 +2,7 @@ import type { Bot } from 'grammy';
 import cron from 'node-cron';
 import { DEFAULT_TIMEZONE } from '@core/config';
 import { ChatbotService } from './chatbot.service';
-import {
-  dailySummary,
-  earthquakeMonitor,
-  emailSummary,
-  exerciseReminder,
-  footballUpdate,
-  makavdiaUpdate,
-  polymarketUpdate,
-  reminderCheck,
-  selfieSummary,
-  sportsCalendar,
-  weeklyExerciseSummary,
-  youtubeCheck,
-} from './schedulers';
+import { dailySummary, earthquakeMonitor, exerciseReminder, footballUpdate, makavdiaUpdate, polymarketUpdate, reminderCheck, sportsCalendar, weeklyExerciseSummary } from './schedulers';
 import { LOOKBACK_MINUTES } from './schedulers/earthquake-monitor';
 
 function createSchedule(expression: string, handler: () => Promise<void>, timezone: string = DEFAULT_TIMEZONE): void {
@@ -29,115 +16,22 @@ export class ChatbotSchedulerService {
   ) {}
 
   init(): void {
-    createSchedule(`00 23 * * *`, async () => {
-      await this.handleDailySummary();
-    });
+    createSchedule(`00 23 * * *`, async () => dailySummary(this.bot, this.chatbotService));
 
-    createSchedule(`59 12,23 * * *`, async () => {
-      await this.handleFootballUpdate();
-    });
+    createSchedule(`59 12,23 * * *`, async () => footballUpdate(this.bot, this.chatbotService));
 
-    createSchedule(`30 9 * * *`, async () => {
-      await this.handleMakavdiaUpdate();
-    });
+    createSchedule(`30 9 * * *`, async () => makavdiaUpdate(this.bot, this.chatbotService));
 
-    createSchedule(`00 10 * * 0,3`, async () => {
-      await this.handleSportsCalendar();
-    });
+    createSchedule(`00 10 * * 0,3`, async () => sportsCalendar(this.bot, this.chatbotService));
 
-    createSchedule(`0 19 * * *`, async () => {
-      await this.handleExerciseReminder();
-    });
+    createSchedule(`0 19 * * *`, async () => exerciseReminder(this.bot, this.chatbotService));
 
-    createSchedule(`0 22 * * 6`, async () => {
-      await this.handleWeeklyExerciseSummary();
-    });
+    createSchedule(`0 22 * * 6`, async () => weeklyExerciseSummary(this.bot, this.chatbotService));
 
-    createSchedule(`35 23 * * *`, async () => {
-      await this.handleEmailSummary();
-    });
+    createSchedule(`15 * * * *`, async () => reminderCheck(this.bot));
 
-    createSchedule(`15 * * * *`, async () => {
-      await this.handleReminderCheck();
-    });
+    createSchedule(`*/${LOOKBACK_MINUTES} * * * *`, async () => earthquakeMonitor(this.bot));
 
-    createSchedule(`*/${LOOKBACK_MINUTES} * * * *`, async () => {
-      await this.handleEarthquakeMonitor();
-    });
-
-    createSchedule(`5 12,16,23 * * *`, async () => {
-      await this.handlePolymarketUpdate();
-    });
-
-    createSchedule(`1 0 * * *`, async () => {
-      await this.handleSelfieSummary();
-    });
-
-    // createSchedule(`12 12,14,16,17,20,22,23 * * *`, async () => {
-    //   await this.handleYoutubeCheck();
-    // });
-
-    setTimeout(() => {
-      // this.handleDailySummary();
-      // this.handleEmailSummary();
-      // this.handleFootballUpdate();
-      // this.handleMakavdiaUpdate();
-      // this.handleSportsCalendar();
-      // this.handleExerciseReminder();
-      // this.handleWeeklyExerciseSummary();
-      // this.handleReminderCheck();
-      // this.handleEarthquakeMonitor();
-      // this.handleYoutubeCheck();
-      // this.handlePolymarketUpdate();
-      // this.handleSelfieSummary();
-    }, 8000);
-  }
-
-  private async handleDailySummary(): Promise<void> {
-    await dailySummary(this.bot, this.chatbotService);
-  }
-
-  private async handleEmailSummary(): Promise<void> {
-    await emailSummary(this.bot, this.chatbotService);
-  }
-
-  private async handleFootballUpdate(): Promise<void> {
-    await footballUpdate(this.bot, this.chatbotService);
-  }
-
-  private async handleMakavdiaUpdate(): Promise<void> {
-    await makavdiaUpdate(this.bot, this.chatbotService);
-  }
-
-  private async handleSportsCalendar(): Promise<void> {
-    await sportsCalendar(this.bot, this.chatbotService);
-  }
-
-  private async handleExerciseReminder(): Promise<void> {
-    await exerciseReminder(this.bot, this.chatbotService);
-  }
-
-  private async handleWeeklyExerciseSummary(): Promise<void> {
-    await weeklyExerciseSummary(this.bot, this.chatbotService);
-  }
-
-  private async handleReminderCheck(): Promise<void> {
-    await reminderCheck(this.bot);
-  }
-
-  private async handleEarthquakeMonitor(): Promise<void> {
-    await earthquakeMonitor(this.bot);
-  }
-
-  private async handleYoutubeCheck(): Promise<void> {
-    await youtubeCheck(this.bot, this.chatbotService);
-  }
-
-  private async handlePolymarketUpdate(): Promise<void> {
-    await polymarketUpdate(this.bot);
-  }
-
-  private async handleSelfieSummary(): Promise<void> {
-    await selfieSummary(this.bot);
+    createSchedule(`5 12,16,23 * * *`, async () => polymarketUpdate(this.bot));
   }
 }
