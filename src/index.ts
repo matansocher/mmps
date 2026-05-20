6b06940244924cf680b23669fdecd1ae2a6693b0
Matan Socher <matansocher@gmail.com>
2026-05-20 14:56:14 +0300

    chore: remove the stacker feature (#388)

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ import { BOT_CONFIG as chilliConfig, initChilli } from '@features/chilli';
 import { BOT_CONFIG as coachConfig, initCoach } from '@features/coach';
 import { initLangly, BOT_CONFIG as langlyConfig } from '@features/langly';
 // import { initMagister, BOT_CONFIG as magisterConfig } from '@features/magister';
-import { initStacker, BOT_CONFIG as stackerConfig } from '@features/stacker';
 import { initWolt, BOT_CONFIG as woltConfig } from '@features/wolt';
 import { initWorldly, BOT_CONFIG as worldlyConfig } from '@features/worldly';
 import { DB_NAME as AUTH_DB_NAME, registerAuthRoutes } from '@shared/auth';
@@ -49,7 +48,6 @@ async function main() {
   shouldInitBot(coachConfig) && (await initCoach(app));
   shouldInitBot(langlyConfig) && (await initLangly());
   // shouldInitBot(magisterConfig) && (await initMagister());
-  shouldInitBot(stackerConfig) && (await initStacker(app));
   shouldInitBot(woltConfig) && (await initWolt(app));
   shouldInitBot(worldlyConfig) && (await initWorldly(app));
 
