634afac9fe7edd7c6fb6007c79dc2777cbb61242
Matan Socher <matansocher@gmail.com>
2026-05-23 13:49:51 +0300

    chore: update token in BOT_CONFIG to a string value
    Change token to a string instead of using environment variable.

--- src/features/world-cup/world-cup.config.ts
+++ src/features/world-cup/world-cup.config.ts
@@ -3,5 +3,5 @@ import type { TelegramBotConfig } from '@services/telegram';
 export const BOT_CONFIG: TelegramBotConfig = {
   id: 'WORLD_CUP',
   name: 'World Cup ⚽🏆',
-  token: process.env.WORLD_CUP_TELEGRAM_BOT_TOKEN!,
+  token: 'WORLD_CUP_TELEGRAM_BOT_TOKEN',
 };
