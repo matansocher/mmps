b647399a2a7cc38534e7d319d890fc4411a87d8b
Matan Socher <matansocher@gmail.com>
2026-06-22 12:48:34 +0300

    chore: update channels list

--- src/services/telegram-client/constants.ts
+++ src/services/telegram-client/constants.ts
@@ -14,14 +14,12 @@ export const LISTEN_TO_EVENTS = [
 ];
 
 export const CHANNELS = {
-  SHIGURIM: { id: '2374433519', name: `התרעות על שיגורים` },
   TOODIE: { id: '1332013273', name: `Toodie 🦔` },
   NEWS_FROM_THE_FIELD: { id: '1406113886', name: `חדשות 100שטח` },
-  ABU_ZALAH: { id: '1425850587', name: `אבו צאלח הדסק הערבי` },
-  INTELLITIMES: { id: '1207026910', name: `INTELLITIMES | إنتل تايمز | אינטלי טיימס` },
   DANIEL_AMRAM: { id: '1613161072', name: `דניאל עמרם ללא צנזורה` },
   SHLOMO_WEATHER: { id: '1665567611', name: `שלמה ⛈️ מזג אוויר` },
-  KVUZA: { id: '1727761169', name: `קבוצה` },
+  MORAD_STERN: { id: '1083698033', name: `מורד שטרן, הערוץ הטכנולוגי 🇮🇱` },
+  GEEKTIME: { id: '1282228958', name: `Geektime` },
 };
 
 export const EXCLUDED_CHANNELS = [];
