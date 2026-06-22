f12661370810eaba2c266ab3930d1c0f7a9bc63c
Matan Socher <matansocher@gmail.com>
2026-06-22 11:42:07 +0300

    Revert "feat(secretary): implement group message monitoring (#442)"
    This reverts commit 2567383c0d3c64902e247c76cc47c83780afd835.

--- src/services/telegram-client/index.ts
+++ src/services/telegram-client/index.ts
@@ -1,5 +1,4 @@
 export { disconnectTelegramClient } from './provide-telegram-client';
-export { CHANNELS } from './constants';
 export * from './types';
 export { listen } from './utils/listen';
 export type { TelegramMessage, ConversationDetails, SenderDetails } from './utils/listen';
