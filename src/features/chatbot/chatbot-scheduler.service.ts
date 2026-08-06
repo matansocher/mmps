885bda69c277f3687005bb81c34f373ec736a90c
Matan Socher <matansocher@gmail.com>
2026-08-06 11:53:56 +0300

    chore: delete orphaned morning-brief scheduler file

--- src/features/chatbot/chatbot-scheduler.service.ts
+++ src/features/chatbot/chatbot-scheduler.service.ts
@@ -42,7 +42,6 @@ export class ChatbotSchedulerService {
   init(): void {
     createSchedule(`00 23 * * *`, async () => dailySummary(this.bot, this.chatbotService));
 
-
     createSchedule(`00 18 * * *`, async () => birthdayReminder(this.bot));
 
     createSchedule(`59 12,23 * * *`, async () => footballUpdate(this.bot, this.chatbotService));
